061a1bdd4db603aefb32e1549a25f7a126e3e680
Vaskrsije Panic <panicvaskrsije@gmail.com>
2023-03-31 23:35:40 +0200

    Odradjeno sve sto je falilo, responsive gotov, dodat novi footer, search, na responsive je fixed menu i dodata pozadina prilikom otvaranja menija

--- js/index.js
+++ js/index.js
@@ -1,58 +1,35 @@
 function search() {
-  var x = document.getElementById("search_content");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+    var x = document.getElementById("searchContent");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
 }
 
-function media_width(x){
-  if (x.matches) {
-    document.getElementsByClassName("row").style.flexDirection = "column";
-    document.getElementsByClassName("nav-bar").style.flexDirection = "column";
-  } else {
-    document.getElementsByClassName("row").style.flexDirection = "row";
-  }
-}
-var x = window.matchMedia("(max-width: 400px)")
-media_width(x) // Call listener function at run time
-x.addListener(media_width) // Attach listener function on state changes
+function showMenu() {
+    var x = document.getElementById("menuList");
+    var open = document.getElementById("menuIconOpen");
+    var close = document.getElementById("menuIconClose");
+    var bg = document.getElementById("bgDark");
+    open.style.display = "none"
+    x.style.display = "flex"
+    close.style.display = "flex"
+    bg.style.display = "block"
 
-Fancybox.bind("[data-fancybox]", {
-    loop: true,
-    buttons: [
-      "zoom",
-      "slideShow",
-      "fullScreen",
-      "thumbs",
-      "close"
-    ],
-    animationEffect: "fade",
-    transitionEffect: "slide",
-    transitionDuration: 600
+}
+function closeMenu() {
+  var x = document.getElementById("menuList");
+  var open = document.getElementById("menuIconOpen");
+  var close = document.getElementById("menuIconClose");
+  var bg = document.getElementById("bgDark");
+  x.style.display = "none"
+  open.style.display = "flex"
+  close.style.display = "none"
+  bg.style.display = "none"
+}
+
+const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+  infinite: false,
+  Navigation: false,
 });
-
-
-// new Glide('.glide', {
-//     type: 'carousel',
-//     startAt: 0,
-//     perView: 3,
-//     focusAt: 'center',
-//     gap: 30,
-//     autoplay: 3000,
-//     hoverpause: true,
-//     animationDuration: 1000,
-//     animationTimingFunc: 'ease-out',
-//     peek: {
-//       before: 50,
-//       after: 50
-//     },
-//     breakpoints: {
-//       768: {
-//         perView: 1
-//       }
-//     },
-//     // Konfiguracija za bullets
-//     nav: '.glide__bullets'
-//   }).mount()
